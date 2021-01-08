7e177ff6e8cde34d56d97fc03a37f22a6e77b700
Michael Schoen <40121916+NWMichl@users.noreply.github.com>
2021-01-08 16:27:52 +0100

    Catch CVSS score float error

--- filter_plugins/custom_adv_filter.py
+++ filter_plugins/custom_adv_filter.py
@@ -13,16 +13,23 @@ class FilterModule(object):
             if (os_loop['json'].get('advisories')):
                 for adv_loop in os_loop['json']['advisories']:
                     
+                    # catch two possible data structures with first fixed releases
                     fixed = []
                     if (adv_loop.get('firstFixed')):
                         fixed = adv_loop['firstFixed']
                     else:
                         fixed = adv_loop['platforms'][0]['firstFixes'][0]['name']
+                        
+                    # catch cvss score string that contains no number
+                    try:
+                        cvss_float = float(adv_loop['cvssBaseScore'])
+                    except ValueError:
+                        cvss_float = 0.0
 
                     output_var.append({ 'os': os_loop['item']['os'], 
                                         'id': adv_loop['advisoryId'],
                                         'sir': adv_loop['sir'],
-                                        'cvss': float(adv_loop['cvssBaseScore']),
+                                        'cvss': cvss_float),
                                         'cve': adv_loop.get('cves', []),
                                         'url': adv_loop['publicationUrl'],
                                         'fixed': fixed
